096c44d7bb82c6d20dbafd0de9d768fc606e41ae
Mizan96 <mizanurcse12@gmail.com>
2024-08-11 07:32:16 +0600

    setting files is edited

--- news/settings.py
+++ news/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6k_!wl)!p8(j_z9$bqd$6cs1!e11tm=du36g91po(p-_6^ue!7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.joinpath('frontend','build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,8 +137,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL =  'static/'
+STATIC_URL = '/static/' 
 MEDIA_URL =  '/images/'
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    BASE_DIR.joinpath('frontend', 'build', 'static')
+]
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
--- news/urls.py
+++ news/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
 
 urlpatterns = [
+    path('', index, name='home-page'),
     path('admin/', admin.site.urls),
     path('api/', include('app_article.urls')),
     path('api-auth/', include('rest_framework.urls')),
@@ -27,4 +33,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

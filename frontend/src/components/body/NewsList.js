66a89f7c28a9772b7d75cc5d980e79878feeb3fb
Mizan96 <mizanurcse12@gmail.com>
2024-08-13 12:20:10 +0600

    author time category is added into article detail

--- frontend/src/components/body/NewsList.js
+++ frontend/src/components/body/NewsList.js
@@ -14,16 +14,6 @@ import {
 function NewsList({image, title, article, id}) {
   return (
     <>
-    {/* <Card id="newsCard">
-      <Card.Img variant="top" src={image} alt={image}/>
-      <Card.Body>
-        <Card.Title>{title.slice(0,40)}</Card.Title>
-        <Card.Text>{ article.slice(0,90) }</Card.Text>
-        <Link to={`/news/detail/${id}/`}><Button variant="primary" >বিস্তারিত</Button></Link>
-      </Card.Body>
-    </Card> */}
-
-
     <MDBCard>
       <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
         <MDBCardImage src={image} fluid alt={image} />
--- frontend/src/screen/NewsDetailsScreen.js
+++ frontend/src/screen/NewsDetailsScreen.js
@@ -4,38 +4,45 @@ import { useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import getArtticleDetailFromServer from '../store/article-detail-action';
-import { Col, Container, Row } from 'react-bootstrap';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol
+} from 'mdb-react-ui-kit';
+
 
 function NewsDetailsScreen() {
   const dispatch = useDispatch();
-  const data = useSelector((state)=> state.articleDetail.data);
-  const {id} = useParams();
+  const data = useSelector((state) => state.articleDetail.data);
+  const { id } = useParams();
   console.log(id)
   console.log(data);
 
-  useEffect(() =>{
+  useEffect(() => {
     dispatch(getArtticleDetailFromServer(id));
-  },[dispatch]);
+  }, [dispatch]);
 
   return (
     <>
-    <TopBar />
-    <Container>
-      <Row>
-        <Col xl={9} lg={9}>
-        {/* <p>{data.category}</p> */}
-        <h1 className='display-6'>{data.title}</h1>
-        <div className='border'></div>
-        <img  className='rounded my-2 mx-auto d-block'  src={data.image} />
-        <p>{data.description}</p>
-        <div className='border'></div>
-        <p className='py-2 text-justify'>{data.article}</p>
-        </Col>
-      </Row>
-    </Container>
-    
-    
-    <Bottom />
+      <TopBar />
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol xl={9} lg={9}>
+            <p className='text-danger py-2'>{data.category.category}</p>
+            <h1 className='display-6'>{data.title}</h1>
+            <p className='py-2'>লেখকঃ <strong>{data.author.name}</strong></p>
+            <p className='py-2'>পোস্ট করা হয়েছেঃ <strong>{data.timesince}</strong></p>
+            <div className='border'></div>
+            <img className='rounded my-2 mx-auto d-block' src={data.image} />
+            <p>{data.description}</p>
+            <div className='border'></div>
+            <p className='py-2 text-justify'>{data.article}</p>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+
+
+      <Bottom />
     </>
   )
 }
